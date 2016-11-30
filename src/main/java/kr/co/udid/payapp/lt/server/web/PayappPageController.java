0783d25b33cdbc343a88a20646b4f31fb83401ed
purred <purred@udid.co.kr>
2016-11-30 15:00:45 +0900

    로컬 테스트 작업중

---
+++ src/main/java/kr/co/udid/payapp/lt/server/web/PayappPageController.java
@@ -0,0 +1,18 @@
+package kr.co.udid.payapp.lt.server.web;
+
+import org.springframework.stereotype.Controller;
+
+import kr.co.udid.payapp.lt.model.payapp.PayappSv;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by RED on 2016-11-30.
+ */
+@Controller
+@RequiredArgsConstructor
+public class PayappPageController
+{
+	private final PayappSv payappSv;
+
+
+}
